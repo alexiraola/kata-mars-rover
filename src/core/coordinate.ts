0c5da43f4d662b3c6fcd0fe8a917e29690e86d03
Alex Iraola <alex.iraola@gmail.com>
2023-12-27 16:03:55 +0100

    Refactor Coordinate to make it easier to read

--- src/core/coordinate.ts
+++ src/core/coordinate.ts
@@ -1,6 +1,6 @@
 export class Coordinate {
-  private maxLatitude = 9;
-  private maxLongitude = 9;
+  private planetWidth = 10;
+  private planetHeight = 10;
 
   constructor(private latitude: number, private longitude: number) { }
 
@@ -9,35 +9,35 @@ export class Coordinate {
   }
 
   increaseLatitude() {
-    const latitude = this.latitude === this.maxLatitude
-      ? 0
-      : this.latitude + 1;
-
+    const latitude = this.increaseValue(this.latitude, this.planetWidth);
     return new Coordinate(latitude, this.longitude);
   }
 
   decreaseLatitude() {
-    const latitude = this.latitude === 0
-      ? this.maxLatitude
-      : this.latitude - 1
-
+    const latitude = this.decreaseValue(this.latitude, this.planetWidth - 1);
     return new Coordinate(latitude, this.longitude);
   }
 
   increaseLongitude() {
-    const longitude = this.longitude === this.maxLongitude
-      ? 0
-      : this.longitude + 1;
-
+    const longitude = this.increaseValue(this.longitude, this.planetHeight - 1);
     return new Coordinate(this.latitude, longitude);
   }
 
   decreaseLongitude() {
-    const longitude = this.longitude === 0
-      ? this.maxLongitude
-      : this.longitude - 1;
-
+    const longitude = this.decreaseValue(this.longitude, this.planetHeight - 1);
     return new Coordinate(this.latitude, longitude);
+  }
+
+  private increaseValue(value: number, limit: number) {
+    return value === limit
+      ? 0
+      : value + 1;
+  }
+
+  private decreaseValue(value: number, limit: number) {
+    return value === 0
+      ? limit
+      : value - 1;
   }
 }
 
